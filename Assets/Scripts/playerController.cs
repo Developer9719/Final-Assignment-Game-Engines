b91dc7ec158e7b83077ff786290104388b54be19
Justin <justin.bat19@gmail.com>
2025-04-10 04:08:27 -0400

    Character Movement with Jump Complete

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -13,6 +13,12 @@ public class playerController : MonoBehaviour
     // User Inputs
     public float hAxis;
     public float vAxis;
+    // Jumping
+    public LayerMask Ground;
+    public float raycastDistance = 0.6f; // Scale of character is 1 so from the middle to the bottom is 0.5, 0.6 will detect anything just below the character
+    private bool isOnGround;
+    public float jump = 100f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,16 +32,32 @@ public class playerController : MonoBehaviour
         hAxis = Input.GetAxis("Horizontal"); // Stores A/D input
         vAxis = Input.GetAxis("Vertical"); // Stores W/S input
 
+        // Ground Check: https://www.youtube.com/watch?v=o8DDsFccxBE 
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance, Ground))
+        {
+            isOnGround = true;
+        } else
+        {
+            isOnGround = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            rb.AddForce(Vector3.up * jump, ForceMode.Impulse); // ForceMode.Impulse force is added instantly
+        }
         movement = new Vector3(hAxis, 0, vAxis); // Creates a vector for 3d motion with the horizontal motion and vertical motion, the 0 means theres no jumping motion
+        
     }
 
     void FixedUpdate() // Sets a force to a rigidbody element in each frame
     {
         move(movement);
+        Debug.Log("Project Gravity: " + Physics.gravity);
     }
 
     void move(Vector3 direction)
     {
-        rb.velocity = direction * movementSpeed; // Sets the velocity (speed and direction) of a rigidbody element
+        rb.AddForce(direction * movementSpeed, ForceMode.VelocityChange); // Character movement with jump
     }
 }
